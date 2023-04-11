43fba45033a4e182c67ab320948ec6e6318b7b69
klack <rlayton@gmail.com>
2023-04-10 20:04:39 -0600

    Incrementing correctly

--- lux_scripts/build_versioning.py
+++ lux_scripts/build_versioning.py
@@ -5,7 +5,7 @@ import os
 import datetime
 tm = datetime.datetime.today()
 
-FILENAME_VERSION = 'Marlin/Lux_version'
+FILENAME_VERSION = 'Marlin\src\inc\Lux_version'
 FILENAME_VERSION_H = 'Marlin\src\inc\Lux_version.h'
 version = ""
 build_no = 0
@@ -13,23 +13,25 @@ build_date = ""
 
 try:
     with open(FILENAME_VERSION) as f:
-        version = f.readline()
+        version = f.readline().strip()
         build_no = int(f.readline())
-        build_date = f.readline()
+        build_date = f.readline().strip()
 except:
     print('Could not find version information')
     build_no = 1
-    version = ""
+    version = "1"
 
-if(os.environ.get('LUX_INCREMENT_BUILD') == "1"):
-  build_no+=1
-  build_date = f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S}"
+if not (os.environ.get('LUX_SKIP_INCREMENT') == "1"):
+    build_no+=1
+    build_date = f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S}"
 
 with open(FILENAME_VERSION, 'w+') as f:
-    f.writelines([version,str(build_no),build_date])
+    f.write(version + "\n")
+    f.write(str(build_no) + "\n")
+    f.write(build_date + "\n")
     print('Build number: {}'.format(build_no))
 
-hf = """
+hf = """ 
 #ifndef BUILD_NUMBER
   #define BUILD_NUMBER "{}"
 #endif
@@ -39,6 +41,6 @@ hf = """
 #ifndef SHORT_BUILD_VERSION
   #define SHORT_BUILD_VERSION "{}"
 #endif
-""".format(build_no, version, str(build_no), date, version)
+""".format(build_no, version, str(build_no), build_date, version)
 with open(FILENAME_VERSION_H, 'w+') as f:
     f.write(hf)
