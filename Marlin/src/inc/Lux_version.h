6a4ff7a20f7c0e80075d48a307299a5f27092213
klack <rlayton@gmail.com>
2023-04-14 22:03:37 -0600

    Fix bmg

--- Marlin/src/inc/Lux_version.h
+++ Marlin/src/inc/Lux_version.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "68"
+  #define BUILD_NUMBER "69"
 #endif
 #ifndef DETAILED_BUILD_VERSION
-  #define DETAILED_BUILD_VERSION "2.1.X.0 build 68 2023-04-14 21:38:25"
+  #define DETAILED_BUILD_VERSION "2.1.X.0 build 69 2023-04-14 22:00:38"
 #endif
 #ifndef SHORT_BUILD_VERSION
   #define SHORT_BUILD_VERSION "2.1.X.0"
