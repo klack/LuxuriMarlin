0d5c836055dbb7a4b958479dcc28479211c2b676
klack <rlayton@gmail.com>
2023-10-13 22:11:37 -0600

    Increase version

--- Marlin/src/inc/Lux_version.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "2"
-#endif
-#ifndef DETAILED_BUILD_VERSION
-  #define DETAILED_BUILD_VERSION "2.1.2.1 Luxuri build 2 2023-08-02 21:02:47"
-#endif
-#ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "2.1.2.1 Luxuri"
-#endif
